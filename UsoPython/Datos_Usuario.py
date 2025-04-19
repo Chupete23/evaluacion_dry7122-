a5dcc69634eb37675028062785931d7184362a37
Chupete23 <changeme@example.com>
2025-04-19 00:09:37 +0000

    Agrego datos de usuario

---
+++ UsoPython/Datos_Usuario.py
@@ -0,0 +1,11 @@
+# Solicitar datos a usuario
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo_seccion = input("Ingrese su código de sección: ")
+sede = input("Ingrese la sede: ")
+
+# Mostrar la información solicitada
+print("Datos del usuario:")
+print(f"Nombre: {nombre} {apellido}")
+print(f"Código-Sección: {codigo_seccion}")
+print(f"Sede: {sede}")
